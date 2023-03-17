5ac740f5e20cef1fb29c28aca548c2512ce4cadd
macapedi <macapedi90@gmail.com>
2023-03-17 11:13:53 -0400

    web routes created

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [TodoController::class, 'index']);
+Route::get('create', [TodoController::class, 'create']);
+
+Route::get('details', [TodoController::class, 'details']);
+Route::get('edit', [TodoController::class, 'edit']);
+Route::post('update', [TodoController::class, 'update']);
+
+Route::get('delete', [TodoController::class, 'delete']);
